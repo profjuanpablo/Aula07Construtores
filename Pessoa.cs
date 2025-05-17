2b49a389aa72da021ead69c87ad71c2d6d1686cb
Juan Pablo <prof.juanpablo@gmail.com>
2025-05-16 21:12:41 -0300

    definição de construtores

---
+++ Pessoa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula07_Construtores
+{
+    class Pessoa
+    {
+
+        //declaração de propriedades da classe Pessoa
+
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        //COnstrutor declarado pelo programador onde passamos os
+        //parâmetros para a instanciação do objeto pessoa.
+
+        public Pessoa(double peso, double altura) {
+            Peso = peso;
+            Altura = altura;
+        }
+
+        public Pessoa(string nome, double peso, double altura): this(peso,altura)
+        {
+            Nome = nome;
+         }
+
+
+        public Pessoa(string nome, int idade, double peso, double altura): this(nome,peso,altura)
+        {
+            Idade = idade;
+        }
+
+
+        //Destrutor
+        ~Pessoa()
+        {
+            Console.WriteLine("Objeto  finalizado");
+            Console.ReadKey();
+        }
+
+        public void GetPessoa() { 
+            Console.WriteLine($"Nome: {Nome}\nIdade: {Idade}" +
+                    $"\nPeso: {Peso}\nAltura: {Altura}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula07_Construtores;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //instanciar um objeto Pessoa
+        Pessoa pessoa = new Pessoa("Pedro Henrique",80,1.86);
+        Pessoa pessoa2 = new Pessoa("Juan Pablo",105,1.87);
+       
+        //pessoa.Nome = "Juan Pablo";
+        //pessoa.Idade = 49;
+        //pessoa.Peso = 105;
+        //pessoa.Altura = 1.87;
+
+        
+
+        pessoa.GetPessoa();
+        Console.WriteLine(new string('-', 20));
+       
+
+        Console.ReadKey();
+    }
+}
